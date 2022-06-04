eced01688e5ee7d7bed1f4498d11b393c67154de
lau17002 <lau17002@byui.edu>
2022-06-04 16:38:29 -0600

    done

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -41,5 +41,23 @@ if (now.getDay() == 3 || now.getDay() == 2){
     banner.style.display = "none";
 }
 
+//Number of visits
+const visitsDisplay = document.querySelector(".visit");
+
+let numVisits = Number(window.localStorage.getItem("visit-ls"));
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
+
+const timeBetween = Math.round((Date.now() - lastVisit) / (1000 * 60 * 60 * 24));
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = timeBetween;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+localStorage.setItem("lastVisit", Date.now());
+localStorage.setItem("visit-ls", numVisits);
+
 
 
